d0284984bc4b2c2aba24dbc31f1e104bb1c63c5e
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-26 23:46:19 -0300

    feat: user repository with specific find by email e cpf created

--- e-commerce.user/src/main/java/com/brunner/e_commerce/user/repositories/UserRepository.java
+++ e-commerce.user/src/main/java/com/brunner/e_commerce/user/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.brunner.e_commerce.user.repositories;
 import com.brunner.e_commerce.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
 
-    public User findUserByCpf(String cpf);
-    public User findUserByEmail(String email);
+    public Optional<User> findUserByCpf(String cpf);
+    public Optional<User> findUserByEmail(String email);
 
 }
