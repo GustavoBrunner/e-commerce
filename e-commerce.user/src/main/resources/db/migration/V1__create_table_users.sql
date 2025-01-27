55f874320eb9c7f2bd3e97cb2c3bba63d4be211e
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-26 23:41:39 -0300

    fix: not null constraint on main address removed

--- e-commerce.user/src/main/resources/db/migration/V1__create_table_users.sql
+++ e-commerce.user/src/main/resources/db/migration/V1__create_table_users.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS users(
     PK_id VARCHAR(100) NOT NULL,
     FK_wallet_id VARCHAR(100) UNIQUE,
-    FK_main_address_id VARCHAR(100) NOT NULL UNIQUE,
+    FK_main_address_id VARCHAR(50)  UNIQUE,
 
     password VARCHAR(200) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
-
+    email VARCHAR(100) NOT NULL UNIQUE,
+    user_type VARCHAR(10) NOT NULL
 
 
 );
--- e-commerce.user/src/main/resources/db/migration/V3__alter_table_users.sql
+++ e-commerce.user/src/main/resources/db/migration/V3__alter_table_users.sql
@@ -2,3 +2,4 @@ ALTER TABLE users
     ADD CONSTRAINT FK_main_address_id
         FOREIGN KEY(FK_main_address_id)
         REFERENCES address(PK_id)
+        ON DELETE SET NULL
